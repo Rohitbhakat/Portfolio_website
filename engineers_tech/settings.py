2eb7a93582e14a5a8529f5a30ee5b0dface19707
Rohitbhakat <60672520+Rohitbhakat@users.noreply.github.com>
2020-11-18 18:46:44 +0530

    Update settings.py

--- engineers_tech/settings.py
+++ engineers_tech/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1%^$!=rwey6_w$p)=txsh964jsaoyu4mja)w$8ikx08@ifmbas'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['139.59.71.156']
 
